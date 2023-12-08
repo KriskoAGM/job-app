e26e10ba8b6d87f6718a902546fde0254e59c7b8
KriskoAGM <christiqn.iordanow96@gmail.com>
2023-12-08 23:59:13 +0200

    changed normal state with a persisted state

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -2,18 +2,14 @@ import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import * as authService from '../services/authService'
-
+import usePersistedState from "../hooks/usePersistedState";
 
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const navigate = useNavigate();
-    const [auth, setAuth] = useState(() => {
-        localStorage.removeItem('accessToken');
-
-        return {};
-    });
+    const [auth, setAuth] = usePersistedState('auth', {});
 
     const [error, setError] = useState('');
 
